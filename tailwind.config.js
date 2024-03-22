3dec3cc16997e09cd6f6a9ea4af1f01652bef8cc
biggieliten <filipisaksson00@gmail.com>
2024-03-22 14:45:39 +0100

    Installed Vite, Tailwind and sass. Theme colors in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      yellow: "#FFDE7D",
+      blue: "#112A46",
+      vanilla: "#F8F3D4",
+    },
     extend: {},
   },
   plugins: [],
