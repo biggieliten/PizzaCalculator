a3e84ba20b854f421e9822f16d828a213292e336
samadamp <94sampan@skola.boras.se>
2024-03-26 10:37:08 +0100

    copied pizzcard

--- src/Components/Cards/PizzaCard.tsx
+++ src/Components/Cards/PizzaCard.tsx
@@ -1,72 +1,45 @@
-/* import Card from "./Cards";
-
-function PizzaCard() {
-  
-
-    return (
-      <>
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="font-bold text-2xl">Välj en pizza</h1>
-      <div className="overflow-auto h-96 w-fit">
-    <h1 className="font-bold text-2xl">Kebab</h1>
-      <div className='flex flex-wrap gap-2 my-1'>
-      <Card title = {"Pizza"} content = {"Ingredienser"} price = {50} imageUrl="\img\kebab.jpeg" />
-      <Card title = {"Pizza"} content = {"Ingredienser"} price = {50} imageUrl="\img\kebab.jpeg" />
-      </div>
-      <div className='flex gap-2 my-1'>
-      <Card title = {"Pizza"} content = {"Ingredienser"} price = {50} imageUrl="public\img\kebab.jpeg" />
-      <Card title = {"Pizza"} content = {"Ingredienser"} price = {50} imageUrl="public\img\kebab.jpeg" />
-      </div>
-
-
-      <h1 className="font-bold text-2xl">Kyckling</h1>
-      <div className='flex gap-2 mt-10'>
-      <Card title = {"Pizza"} content = {"Ingredienser"} price = {50} imageUrl="public\img\kebab.jpeg" />
-      <Card title = {"Pizza"} content = {"Ingredienser"} price = {50} imageUrl="public\img\kebab.jpeg" />
-      </div>
-      <div className='flex gap-2 my-1'>
-      <Card title = {"Pizza"} content = {"Ingredienser"} price = {50} imageUrl="public\img\kebab.jpeg" />
-      <Card title = {"Pizza"} content = {"Ingredienser"} price = {50} imageUrl="public\img\kebab.jpeg" />
-      </div>
-      
-      <h1 className="font-bold text-2xl">Vegan</h1>
-      <div className='flex gap-2 mt-10'>
-      <Card title = {"Pizza"} content = {"Ingredienser"} price = {50} imageUrl="public\img\kebab.jpeg" />
-      <Card title = {"Pizza"} content = {"Ingredienser"} price = {50} imageUrl="public\img\kebab.jpeg" />
-      </div>
-      <div className='flex gap-2 my-1'>
-      <Card title = {"Pizza"} content = {"Ingredienser"} price = {50} imageUrl="public\img\kebab.jpeg" />
-      <Card title = {"Pizza"} content = {"Ingredienser"} price = {50} imageUrl="public\img\kebab.jpeg" />
-      </div>
-
-
-      </div>
-      </div>
-      </>)
-    ;
-  }
-  
-  export default PizzaCard; */
-
-import Card from "./Cards";
-import { useContext } from "react";
-import { PizzaContext } from "../GlobalPizza/GlobalPizza";
+import Card from "./Cards"
+import { useContext } from "react"
+import { PizzaContext } from "../GlobalPizza/GlobalPizza"
 
 const PizzaCard = () => {
-  const pizzor = useContext(PizzaContext);
+  const pizzor = useContext(PizzaContext)
+
+  const standardPizza = pizzor.filter((pizza)=> pizza.type ==="standard")
+  const kebabPizza = pizzor.filter((pizza)=> pizza.type ==="kebab")
+  const kycklingPizza = pizzor.filter((pizza)=> pizza.type ==="kyckling")
 
   return (
     <>
-      {pizzor.map((pizza) => {
-        return (
-          <Card
-            name={pizza.name}
-            ingredients={pizza.ingredients}
-            price={pizza.price}
-          />
-        );
-      })}
+
+    <div className="overflow-auto h-96 w-fit gap-4 mt-6">
+    <div className="standardpizza ">
+      <h1 className="font-bold">Pizza Klass 1</h1>
+    {standardPizza.map((pizza)=>{
+      return( 
+        <Card name={pizza.name} ingredients={pizza.ingredients} price={pizza.price}/>
+      )})}
+    </div>
+    <div className="kebabpizza ">
+      <h1 className="font-bold">Pizza Klass 2</h1>
+    {kebabPizza.map((pizza)=>{
+      return(  
+        <Card name={pizza.name} ingredients={pizza.ingredients} price={pizza.price}/>
+      )})}
+    </div>
+    <div className="kycklingpizza">
+      <h1 className="font-bold">Pizza Klass 33</h1>
+    {kycklingPizza.map((pizza)=>{
+      return(  
+        <Card name={pizza.name} ingredients={pizza.ingredients} price={pizza.price}/>
+      )})}
+    </div>
+    </div>
+    
+    
+    
     </>
-  );
-};
+  )
+
+}
 export default PizzaCard;
