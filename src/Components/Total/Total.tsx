ee1ca9060201a75090a2bb7bc682c213574fea9f
Robin <robin_nygren@outlook.com>
2024-03-26 10:21:35 +0100

    Trying out to add pizza hardcoded

--- src/Components/Total/Total.tsx
+++ src/Components/Total/Total.tsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { useContext } from "react";
 import { PizzaContext } from "../GlobalPizza/GlobalPizza";
+import PizzaDetails from "./PizzaDetails/PizzaDetails";
+import Button from "../Button/Button";
 
-const TotalSum: React.FC = () => {
+const TotalSum: React.FC = (): React.ReactNode => {
   const pizzas = useContext(PizzaContext);
+  const [pizzaCount, setPizzaCount] = useState(0);
 
   const total = pizzas.reduce((sum, pizza) => sum + pizza.price, 0);
 
+  console.log({ pizzas });
+
+  const AddPizza = () => {
+    const newPizza = {
+      name: "Hawaii",
+      type: "regular",
+      ingredients: ["tomatsås", "ost", "skinka", "ananas"],
+      price: 100,
+    };
+    setPizzaCount(pizzaCount + 1);
+    pizzas.push(newPizza);
+  };
+
   return (
     <div className="place-self-auto ...">
+      {pizzas.map((pizza, index) => (
+        <PizzaDetails key={index} pizza={pizza} />
+      ))}
       <h2>Totala beloppet: {total} kr</h2>
+      <Button ButtonClick={AddPizza} ButtonTitle="Lägg till pizza" />
     </div>
   );
 };
