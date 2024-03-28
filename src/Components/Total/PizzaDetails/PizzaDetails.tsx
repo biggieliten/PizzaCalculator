7c6dd86cac897ef9f1dea527898568a4427281e1
biggieliten <filipisaksson00@gmail.com>
2024-03-28 11:42:01 +0100

    separated total and pizzadetails

--- src/Components/Total/PizzaDetails/PizzaDetails.tsx
+++ src/Components/Total/PizzaDetails/PizzaDetails.tsx
@@ -1,15 +1,23 @@
 import { useContext } from "react";
 import { Pizza } from "../../../pizzaTypes";
 import Button from "../../Button/Button";
-
+import { PizzaContext } from "../../GlobalPizza/GlobalPizza";
 
 const PizzaDetails = ({ pizza }: { pizza: Pizza }) => {
   const logDetails = () => console.log(pizza);
   console.log("Pizza Details:", pizza.name, pizza.ingredients);
+  const { modifiedPizza } = useContext(PizzaContext);
 
   return (
     <div className="">
       <h3>{pizza.name}</h3>
+      {modifiedPizza.map((pizza, index) => (
+        <div key={index} className="mb-2">
+          <h3>{pizza.name}</h3>
+          <p>Price: {pizza.price} kr</p>
+          <i>Toppings: {pizza.toppings && pizza.toppings.join(", ")}</i>
+        </div>
+      ))}
       <ul>
         {/* {pizza.ingredients.map((ingredients, index) => (
           <li key={index}>{ingredients}</li>
--- src/Components/Total/Total.tsx
+++ src/Components/Total/Total.tsx
@@ -33,16 +33,20 @@ import PizzaDetails from "./PizzaDetails/PizzaDetails";
 
 const TotalSum: React.FC = (): React.ReactNode => {
   const { modifiedPizza } = useContext(PizzaContext);
-
   return (
     <div className="flex flex-col w-1/3 p-4 border border-gray-200">
+      <h1 className="font-bold">Kundvagn</h1>
+
       {modifiedPizza.map((pizza, index) => (
+        <PizzaDetails pizza={pizza} key={index} />
+      ))}
+      {/* {modifiedPizza.map((pizza, index) => (
         <div key={index} className="mb-2">
           <h3>{pizza.name}</h3>
           <p>Price: {pizza.price} kr</p>
           <p>Toppings: {pizza.toppings && pizza.toppings.join(", ")}</p>
         </div>
-      ))}
+      ))} */}
       <h2>
         Totala beloppet:{" "}
         {modifiedPizza.reduce((sum, pizza) => sum + pizza.price, 0)} kr
