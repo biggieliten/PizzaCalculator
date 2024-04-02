acce0c1f02ff69572e269f4e2e1485d26c697e08
Robin <Robin_Nygren@outlook.com>
2024-04-02 12:44:13 +0200

    updated footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div>
-      <footer className="bg-blue text-vanilla text-5xl text-center">
+      <footer className="bg-blue py-2 text-vanilla text-5xl text-center">
         KÖP 3 BETALA FÖR 4 - KÖP 3 BETALA FÖR 4 - KÖP 3 BETALA FÖR 4
       </footer>
     </div>
